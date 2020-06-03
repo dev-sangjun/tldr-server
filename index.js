afca3b21f8c8039d88cdc4dc4ff24e2d8a2eff39
ohyeyoye <33212292+ohyeyoye@users.noreply.github.com>
2020-06-03 16:32:57 +0900

    user model

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const connection = require("./db");
+const connection = require("./db/connection");
 const app = express();
 const port = process.env.PORT || 5000;
 
