09ffc1eb5627347f966c6235d22a28f6e61fea99
ohyeyoye <33212292+ohyeyoye@users.noreply.github.com>
2020-06-03 16:43:04 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,20 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose"),
+  bcrypt = require("bcrypt"),
+  Schema = mongoose.Schema,
+  SALT_ROUNDS = 10;
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true, trim: true },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    const hashPassword = await hashPassword(user.password);
+    user.password = hashPassword;
+    next();
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
