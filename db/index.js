cd9cfa0d6d651a15564478bd9cc3fb296df3705b
ohyeyoye <33212292+ohyeyoye@users.noreply.github.com>
2020-06-03 11:24:26 +0900

    mongodb atlas connection successful

---
+++ db/index.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+module.exports = mongoose.connection;
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 require("dotenv").config();
-
+const connection = require("./db");
 const app = express();
 const port = process.env.PORT || 5000;
+
+/*
+  CONNECT TO MONGODB
+*/
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully.");
+});
 
 app.listen(port, () => console.log(`Server running at port: ${port}`));
